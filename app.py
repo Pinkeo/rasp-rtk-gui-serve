dca9c70e11740e0cec03debc51c518af7c96e07a
Pinkeo <pinnepinny@gmail.com>
2024-06-11 10:53:35 +0700

    update client and server

--- app.py
+++ app.py
@@ -1,12 +1,33 @@
-from flask import Flask, render_template, url_for, redirect, request, jsonify
+from flask import Flask, render_template, jsonify
 from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
 socketio = SocketIO(app)
 
+gps_data = {}
+
 @app.route('/')
 def home():
     return render_template('index.html')
+
+@app.route('/gps_data', methods=['GET'])
+def get_gps_data():
+    return jsonify(gps_data)
+
+@socketio.on('connect')
+def handle_connect():
+    print('Client connected')
+
+@socketio.on('disconnect')
+def handle_disconnect():
+    print('Client disconnected')
+
+@socketio.on('gps_data')
+def handle_gps_data(json_data):
+    global gps_data
+    gps_data = json_data
+    print('Received GPS data:', gps_data)  # Log received GPS data
+    emit('update_map', gps_data, broadcast=True)
 
 if __name__ == '__main__':
     socketio.run(app, debug=True)
